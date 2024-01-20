952f6fa35766829294956c587e416b5ac29f9507
HannaWang003 <hannawang003@gmail.com>
2024-01-21 07:53:49 +0800

    匯出sql檔

---
+++ sql/index220.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- 主機： localhost
+-- 產生時間： 2024 年 01 月 20 日 23:52
+-- 伺服器版本： 10.3.38-MariaDB-0ubuntu0.20.04.1
+-- PHP 版本： 7.4.3-4ubuntu2.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `s1120403`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `index220`
+--
+
+CREATE TABLE `index220` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` text DEFAULT NULL,
+  `about` text DEFAULT NULL,
+  `skill` text DEFAULT NULL,
+  `package` text DEFAULT NULL,
+  `url` text DEFAULT NULL,
+  `effect` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `index220`
+--
+ALTER TABLE `index220`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `index220`
+--
+ALTER TABLE `index220`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
