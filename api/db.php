1c1cc2c54dee8f85d5b05671795d4f3bd1ccfd32
HannaWang003 <hannawang003@gmail.com>
2024-01-21 08:50:40 +0800

    設定function __construct()

--- api/db.php
+++ api/db.php
@@ -16,6 +16,10 @@ Class DB{
     protected $dsn="mysql:host=localhost;charset=utf8;dbname=index220";
     protected $pdo;
     protected $table;
+    function __construct($table){
+        $this->table = $table;
+        $this->pdo = new PDO($this->dsn,'root','');
+    }
 
 }
 ?>
