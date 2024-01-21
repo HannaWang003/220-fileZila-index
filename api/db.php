2000c4018a3c63fb8709a76531676103432bf636
HannaWang003 <hannawang003@gmail.com>
2024-01-21 09:00:21 +0800

    請整fuction位置

--- api/db.php
+++ api/db.php
@@ -25,7 +25,43 @@ class DB
         $this->pdo = new PDO($this->dsn, 'root', '');
         // $this->pdo = new PDO($this->dsn, 's1120403', 's1120403');
     }
-    function all($where = '', $other = '')
+    private function a2s($where)
+    {
+        foreach ($where as $key => $val) {
+            $tmp[] = "`$key`='$val'";
+        }
+        return $tmp;
+    }
+    private function sql_all($sql, $where, $other)
+    {
+        if (is_array($where)) {
+            if (!empty($where)) {
+                $sql .= " where " . join(" && ", $this->a2s($where));
+            }
+        } else {
+            $sql .= $where;
+        }
+        $sql .= $other;
+        return $sql;
+    }
+    private function math($math, $col, $array = '', $other = '')
+    {
+        $sql = "select $math(`$col`)  from `$this->table` ";
+        $sql = $this->sql_all($sql, $array, $other);
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+    function find($where)
+    {
+        $sql = "select * from $this->table ";
+        if (is_array($where)) {
+            $tmp = $this->a2s($where);
+            $sql .= " where " . join(" && ", $tmp);
+        } elseif (is_numeric($where)) {
+            $sql .= " where `id`=" . $where;
+        }
+        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+    }
+       function all($where = '', $other = '')
     {
         if (isset($this->table) && !empty($this->table)) {
             $sql = "select * from `$this->table` ";
@@ -33,17 +69,12 @@ class DB
             return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
         }
     }
+
     function count($where = '', $other = '')
     {
         $sql = "select count(*) from `$this->table` ";
         $sql = $this->sql_all($sql, $where, $other);
         return  $this->pdo->query($sql)->fetchColumn();
-    }
-    private function math($math, $col, $array = '', $other = '')
-    {
-        $sql = "select $math(`$col`)  from `$this->table` ";
-        $sql = $this->sql_all($sql, $array, $other);
-        return $this->pdo->query($sql)->fetchColumn();
     }
     function sum($col = '', $where = '', $other = '')
     {
@@ -56,17 +87,6 @@ class DB
     function min($col, $where = '', $other = '')
     {
         return  $this->math('min', $col, $where, $other);
-    }
-    function find($where)
-    {
-        $sql = "select * from $this->table ";
-        if (is_array($where)) {
-            $tmp = $this->a2s($where);
-            $sql .= " where " . join(" && ", $tmp);
-        } elseif (is_numeric($where)) {
-            $sql .= " where `id`=" . $where;
-        }
-        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
     function save($array)
     {
@@ -97,42 +117,12 @@ class DB
         }
         return $this->pdo->exec($sql);
     }
-    private function a2s($where)
-    {
-        foreach ($where as $key => $val) {
-            $tmp[] = "`$key`='$val'";
-        }
-        return $tmp;
-    }
-    private function sql_all($sql, $where, $other)
-    {
-        if (is_array($where)) {
-            if (!empty($where)) {
-                $sql .= " where " . join(" && ", $this->a2s($where));
-            }
-        } else {
-            $sql .= $where;
-        }
-        $sql .= $other;
-        return $sql;
-    }
     function q($sql)
     {
         return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
+
 }
 //資料表
-$Title = new DB('title');
-$Admin = new DB('admin');
-$Music = new DB('music');
-$Concert = new DB('concert');
-$Gallery = new DB('gallery');
-$News = new DB('news');
+
 //判斷是否瀏覽過
-if (isset($_GET['do'])) {
-    if (isset(${ucfirst($_GET['do'])})) {
-        $DB = ${ucfirst($_GET['do'])};
-    }
-} else {
-    $DB = $Title;
-}
