d8bb118825ed356b3085a6d4f16f9095c630cc3a
HannaWang003 <hannawang003@gmail.com>
2024-01-21 08:33:21 +0800

    設定時間

--- api/db.php
+++ api/db.php
@@ -1,24 +1,3 @@
 <?php
 date_default_timezone_set('Asia/Taipei');
-session_start();
-function dd($ary){
-    echo "<pre>";
-    print_r($ary);
-    echo "</pre>";
-}
-function to($url){
-    header("location:$url");
-}
-class DB{
-private $pdo;
-private $dsn = "mysql:host=localhost;chartset=utf8;dbname=s1120403";
-private $table;
-
-public function __construct($table){
-    $this->pdo =new PDO($this->dsn,'root','');
-    $this->table = $table;
-
-}
-}
-
 ?>
