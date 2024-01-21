905f2f5f90d000db037dadd3a94979a531ea23a7
HannaWang003 <hannawang003@gmail.com>
2024-01-21 08:39:25 +0800

    建立連結路徑function to

--- api/db.php
+++ api/db.php
@@ -1,9 +1,15 @@
 <?php
 date_default_timezone_set('Asia/Taipei');
 session_start();
-function dd($ary){
+
+function dd($ary)
+{
     echo "<pre>";
     print_r($ary);
     echo "</pre>";
+}
+function to($url)
+{
+    header("location:$url");
 }
 ?>
