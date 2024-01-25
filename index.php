10fc21550144046f604f51ab7afbf98f7241048c
Hanna <hannawang003@gmail.com>
2024-01-25 09:08:40 +0800

    版排

--- index.php
+++ index.php
@@ -1,21 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>...</title>
     <link rel="stylesheet" href="./dist/bootstrap.min.css" />
     <link rel="stylesheet" href="./dist/locomotive-scroll.min.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
-  </head>
-  <style>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+</head>
+<style>
 
-  </style>
-  <body data-scroll-container>
-   
+</style>
+
+<body data-scroll-container>
+    <div class="container">
+        <!-- myself -->
+        <div class="row">
+            <div class="col">
+                <div class="box01"></div>
+            </div>
+            <!-- /myself -->
+            <!-- work -->
+            <div class="row">
+                <div class="col"></div>
+                <div class="col"></div>
+            </div>
+            <!-- /work -->
+        </div>
+    </div>
+
 
     <script src="./dist/bootstrap.bundle.min.js"></script>
     <script src="./dist/bootstrap.min.js"></script>
@@ -23,10 +40,11 @@
     <script src="./dist/locomotive-scroll.min.js"></script>
 
     <script>
-      let scroll = new LocomotiveScroll({
+    let scroll = new LocomotiveScroll({
         el: document.querySelector("[data-scroll-container]"),
         smooth: true,
-      });
+    });
     </script>
-  </body>
+</body>
+
 </html>
