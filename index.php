6598653db77d1905adb84569debf374a8d413e6e
Hanna <hannawang003@gmail.com>
2024-01-31 11:10:54 +0800

    更新版面

--- index.php
+++ index.php
@@ -13,52 +13,52 @@
     <link href="https://fonts.googleapis.com/css2?family=Anton&family=Righteous&display=swap" rel="stylesheet">
 </head>
 <style>
-    * {
-        margin: 0;
-        box-sizing: border-box;
-    }
+* {
+    margin: 0;
+    box-sizing: border-box;
+}
 
-    body {
-        background-color: #E8E9F3;
-    }
+body {
+    background-color: #E8E9F3;
+}
 
-    #type-container01 {
-        font-size: 10vw;
-    }
+#type-container01 {
+    font-size: 10vw;
+}
 
-    #type-container02 {
-        font-size: 5vw;
-    }
+#type-container02 {
+    font-size: 5vw;
+}
 
-    #type-container03 {
-        display: inline-block;
-        width: 100%;
-        text-align: end;
-        font-size: 2vw;
-    }
+#type-container03 {
+    display: inline-block;
+    width: 100%;
+    text-align: end;
+    font-size: 2vw;
+}
 
-    /* test */
-    .box {
-        border: 1px solid black;
-        height: 60vh;
-        background-color: #E8E9F3;
+/* test */
+.box {
+    border: 1px solid black;
+    height: 60vh;
+    background-color: #E8E9F3;
 
-    }
+}
 
-    .fdV {
-        writing-mode: vertical-lr;
-    }
+.fdV {
+    writing-mode: vertical-lr;
+}
 
-    .fsType {
-        font-family: "Anton", sans-serif;
-        font-weight: 400;
-        font-style: normal;
-    }
+.fsType {
+    font-family: "Anton", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+}
 
-    .fsType01 {
-        font-family: 'Righteous', sans-serif;
+.fsType01 {
+    font-family: 'Righteous', sans-serif;
 
-    }
+}
 </style>
 
 <body data-scroll-container>
@@ -83,28 +83,28 @@
             <!-- FRONT -->
             <span class="fsType fs-1 text-end" data-scroll data-scroll-speed=-2>FRONT</span>
             <span class="fsType fs-1 text-end" data-scroll data-scroll-speed=2>END</span>
-            <div class="col-12 col-sm-6 box" data-scroll data-scroll-direction="horizontal" data-scroll-speed=-2 style="mix-blend-mode: difference;">
+            <div class="col-12 col-sm-6 box" data-scroll data-scroll-direction="horizontal" data-scroll-speed=-2>
                 <div class="col-6">
                     <h2 class="fsType01">CB Coffe</h2>
                     <h4 class="fsType">USAGE</h4>
                 </div>
                 <div class="col-6"></div>
             </div>
-            <div class="col-12 col-sm-6 box" data-scroll data-scroll-direction="vertical" data-scroll-speed=-2 style="mix-blend-mode: difference;">
+            <div class="col-12 col-sm-6 box" data-scroll data-scroll-direction="vertical" data-scroll-speed=-2>
                 <div class="col-6">
                     <h2 class="fsType01">Zoo</h2>
                     <h4 class="fsType">USAGE</h4>
                 </div>
                 <div class="col-6"></div>
             </div>
-            <div class="col-12 col-sm-6 box" data-scroll data-scroll-direction="horizontal" data-scroll-speed=2 style="mix-blend-mode: difference;">
+            <div class="col-12 col-sm-6 box" data-scroll data-scroll-direction="horizontal" data-scroll-speed=2>
                 <div class="col-6">
                     <h2 class="fsType01">Halloween</h2>
                     <h4 class="fsType">USAGE</h4>
                 </div>
                 <div class="col-6"></div>
             </div>
-            <div class="col-12 col-sm-6 box" data-scroll data-scroll-direction="vertical" data-scroll-speed=2 style="mix-blend-mode: difference;">
+            <div class="col-12 col-sm-6 box" data-scroll data-scroll-direction="vertical" data-scroll-speed=2>
                 4
             </div>
             <!-- /FRONT -->
@@ -153,40 +153,40 @@
     <script src="./dist/locomotive-scroll.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
     <script>
-        //locomotiveScroll
-        let scroll = new LocomotiveScroll({
-            el: document.querySelector("[data-scroll-container]"),
-            smooth: true,
-        });
-        //typed
-        let head01 = {
-            strings: ["WEB<br>PLAYER", "WEB<br>ENGINEER"],
-            typeSpeed: 50,
-            backSpeed: 25,
-            onComplete: function() {
-                typed.cursor.style.display = "none"
-                new Typed("#type-container02", head02)
-            }
-            // backSpeed: 50,
-            // loop: true,
+    //locomotiveScroll
+    let scroll = new LocomotiveScroll({
+        el: document.querySelector("[data-scroll-container]"),
+        smooth: true,
+    });
+    //typed
+    let head01 = {
+        strings: ["WEB<br>PLAYER", "WEB<br>ENGINEER"],
+        typeSpeed: 50,
+        backSpeed: 25,
+        onComplete: function() {
+            typed.cursor.style.display = "none"
+            new Typed("#type-container02", head02)
         }
-        let head02 = {
-            strings: ["IDEAS To Bytes"],
-            typeSpeed: 50,
-            onComplete: function() {
-                new Typed("#type-container03", head03)
-            }
+        // backSpeed: 50,
+        // loop: true,
+    }
+    let head02 = {
+        strings: ["IDEAS To Bytes"],
+        typeSpeed: 50,
+        onComplete: function() {
+            new Typed("#type-container03", head03)
+        }
+
+    }
+    let head03 = {
+        strings: ["CONTACT ME <span style='writing-mode:vertical-lr'> =) </span>"],
+        typeSpeed: 100,
+        showCursor: false,
+        onComplete: function() {
 
         }
-        let head03 = {
-            strings: ["CONTACT ME <span style='writing-mode:vertical-lr'> =) </span>"],
-            typeSpeed: 100,
-            showCursor: false,
-            onComplete: function() {
-
-            }
-        }
-        let typed = new Typed("#type-container01", head01);
+    }
+    let typed = new Typed("#type-container01", head01);
     </script>
 </body>
 
